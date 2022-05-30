05a49e8e87f94916d38b788127a16a7b41ea122a
kaspeya <89647596+kaspeya@users.noreply.github.com>
2022-05-30 22:08:33 +0300

    add app api

---
+++ yatube_api/api/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import get_object_or_404
+from posts.models import Group, Post
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .permissions import IsAuthorOrIsAuthenticated
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrIsAuthenticated, IsAuthenticated)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrIsAuthenticated, IsAuthenticated)
+
+    def get_post(self):
+        return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=self.get_post()
+        )
+
+    def get_queryset(self):
+        return self.get_post().comments
