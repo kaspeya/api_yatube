7dd87d2c07d35299cb3e592a84c54ed9fa9080c3
kaspeya <89647596+kaspeya@users.noreply.github.com>
2022-05-31 19:42:01 +0300

    fix after review_2

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -4,19 +4,15 @@ from rest_framework import routers
 from .views import CommentViewSet, GroupViewSet, PostViewSet
 
 app_name = 'api'
-# Создаётся роутер
 router = routers.DefaultRouter()
-# Вызываем метод .register с нужными параметрами
-router.register(r'api/v1/posts', PostViewSet)
-router.register(r'api/v1/groups', GroupViewSet)
+
+router.register('posts', PostViewSet)
+router.register('groups', GroupViewSet)
 router.register(
-    r'api/v1/posts/(?P<post_id>\d+)/comments',
+    r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
-
 urlpatterns = [
-    # Все зарегистрированные в router пути доступны в router.urls
-    # Включим их в головной urls.py
-    path('', include(router.urls)),
+    path('v1/', include(router.urls)),
 ]
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.authtoken import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls', namespace='api')),
+    path('api/', include('api.urls')),
     path('api/v1/api-token-auth/', views.obtain_auth_token),
 ]
 
